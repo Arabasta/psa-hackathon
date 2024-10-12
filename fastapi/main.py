f99e3a7dd6f605196b72a45011d0132539108489
zephyrdark <zephyrdark@users.noreply.github.com>
2024-10-12 20:15:36 +0800

    Add fastapi scheduler to rotate 'Bomen of the day' image, and skeleton code for ML processing

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,42 +1,90 @@
+import os
 import logging
+import random
+from pathlib import Path
+
 from fastapi import FastAPI
-# from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# Load
+# Initialise FastAPI and logger
 app = FastAPI(docs_url="/documentation", redoc_url=None)
 logger = logging.getLogger('uvicorn')
 
-'''
-Constants
-'''
-# AWS
-# AWS_S3_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID')
-# AWS_S3_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
-# AWS_S3_PREDICTION_BUCKET_NAME = os.getenv('AWS_S3_PREDICTION_BUCKET_NAME')
-# AWS_S3_MODEL_BUCKET_NAME = os.getenv('AWS_S3_MODEL_BUCKET_NAME')
-
-UPLOADED_IMAGE_QUEUE = []
+# Constants
+CURRENT_BOMEN_NAME = ""
+RAW_IMAGE_DIR = str(Path(__file__).parent)+"/raw_image"
+RENDERED_IMAGE_DIR = str(Path(__file__).parent)+"/rendered_image"  # todo: remove dummy images
+JSON_DIR = str(Path(__file__).parent)+"/json"
 
 
-@app.put("/api/v1/queue_uploaded_image")
-def queue_uploaded_image(image_name: str):
-    if image_name in UPLOADED_IMAGE_QUEUE:
-        return
-    UPLOADED_IMAGE_QUEUE.append(image_name)
-    return
+# Note: before calling this api, ensure that the raw images are already saved into RAW_IMAGE_DIR
+@app.put("/api/v1/process_raw_image_from_dir")
+def process_raw_image_from_dir(image_name: str) -> bool:
+    try:
+        raw_image_names = Utils.get_files_with_paths(RAW_IMAGE_DIR, ".png")
+        if image_name not in raw_image_names:
+            raise Exception(image_name+" not in "+RAW_IMAGE_DIR)
 
+        if generate_image(image_name):
+            generate_json(image_name)
+            score = str(calculate_oks_score(image_name, CURRENT_BOMEN_NAME))
+            # append score to image_name. e.g., x/12345_image_name.png -> x/12345_image_name_99.png
+            os.rename(image_name+'.png', image_name+'_'+score+'.png')
+            return True
+        else:
+            raise Exception(image_name+" keypoint generation failed ")
 
-def process_queue():
-    # check if queue is empty and calls generate_image and generate_json
-    return
+    except Exception as e:
+        return False
 
 
 def generate_image(image_name: str) -> bool:
-    # attempts to generate image using OpenPose and returns true if successful.
+    # attempts to generate image using OpenPose
+    # if successful, store image at RENDERED_IMAGE_DIR
+    #       return true
     return False
 
 
 def generate_json(image_name: str) -> bool:
-    # attempts to generate json using OpenPose and returns true if successful.
+    # attempts to generate json using OpenPose
+    # if successful, store json at JSON_DIR
+    #       return true
     return False
+
+
+def calculate_oks_score(image_name, current_bomen_name) -> float:
+    # retrieve json of image_name and current_bomen_name from JSON_DIR
+    # perform calculation
+    return -1
+
+
+def change_current_bomen():
+    global CURRENT_BOMEN_NAME
+    logger.info(CURRENT_BOMEN_NAME)
+    all_rendered_image_names = Utils.get_files_with_paths(RENDERED_IMAGE_DIR, ".png")
+    other_rendered_image_names = [x for x in all_rendered_image_names if x != CURRENT_BOMEN_NAME]
+    CURRENT_BOMEN_NAME = random.choice(other_rendered_image_names)
+    return
+
+
+@app.on_event("startup")
+def startup_event():
+    scheduler = BackgroundScheduler(logger=logger)
+    # scheduler to rotate Bo-Men image every X minutes/hours
+    scheduler.add_job(change_current_bomen, 'interval', seconds=1)  # todo: change interval to 1 'minutes' during demo.
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+
+class Utils:
+    @staticmethod
+    def get_files_with_paths(directory, file_extension) -> [str]:
+        file_paths = {}
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith(f"{file_extension}"):  # e.g. '.pkl' files
+                    full_path = os.path.join(root, file)
+                    file_paths[file] = full_path
+        return file_paths
